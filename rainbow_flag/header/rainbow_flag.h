03f2ea96b1b4705c8e608d13656601fa498e5dc3
Melodie Bernard <mbernard@z3r6p1.42lyon.fr>
2024-02-16 13:45:44 +0100

    put rainbow into rainbow

---
+++ rainbow_flag/header/rainbow_flag.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/15 16:44:11 by mbernard          #+#    #+#             */
+/*   Updated: 2024/02/16 13:21:25 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+#endif
---
+++ rainbow_flag/rainbow_flag.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rainbow_flag.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbernard <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 14:10:35 by mbernard          #+#    #+#             */
+/*   Updated: 2024/02/16 13:26:32 by mbernard         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "mlx.h"
+#include "rainbow_flag.h"
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+void	square(t_data *img, int start, int side, int color)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < side)
+	{
+		y = 0;
+		while (y < side)
+		{
+			my_mlx_pixel_put(img, start + x, y, color);
+			y++;
+		}
+		x++;
+	}
+}
+
+void	rectangle_100x600(t_data *img, int start, int color)
+{
+	int	width;
+    int	height;
+	int	x;
+	int	y;
+
+	x = 0;
+	width = 600;
+    height = 100;
+	while (x < width)
+	{
+		y = 0;
+		while (y < height)
+		{
+			my_mlx_pixel_put(img, x, start + y, color);
+			y++;
+		}
+		x++;
+	}
+}
+
+void	rainbow(t_data *img)
+{
+    rectangle_100x600(img, 0, 0xde0000);
+    rectangle_100x600(img, 100, 0xf78900);
+    rectangle_100x600(img, 200, 0xf7e700);
+    rectangle_100x600(img, 300, 0x027d20);
+    rectangle_100x600(img, 400, 0x004af7);
+    rectangle_100x600(img, 500, 0x730084);
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_window;
+	t_data	img;
+
+	mlx = mlx_init();
+	img.img = mlx_new_image(mlx, 600, 600);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
+			&img.endian);
+	mlx_window = mlx_new_window(mlx, 600, 600, "My First Rainbow");
+	rainbow(&img);
+	mlx_put_image_to_window(mlx, mlx_window, img.img, 0, 0);
+	mlx_loop(mlx);
+	return (0);
+}
